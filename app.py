df4c488ae78f3b381a5a4f5ab84295137b56d228
angedev07 <angelgaran20@gmail.com>
2023-11-30 13:27:17 -0500

    feat: Se realizan las siguientes implementacion:
    - Se crea endpoint para retornar la lista de libros apartir de la foto del usuario.
    - Se refactoriza la clase SistemaRecomendacionHibrido para contener todo los metodos.
    - Se agrega una pequenia base de datos de fotos
    - Se actualiza el requirements.txt
    - Se suba la base de datos actualizada.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import sqlite3 as sql 
+import sqlite3 as sql
 import base64
-from pydantic import BaseModel
+from httpResponses.recognition import RecognitionHttp
+from utilsPersonal.save_img import writeImg
+from recognition import IAReconocimiento
+from hybrid import SistemaRecomendacionHibrido
+
 
 app = FastAPI()
-connections = sql.connect('database.db')
+recog = IAReconocimiento()
+system_recomendation = SistemaRecomendacionHibrido()
 
 origins = [
     "http://localhost:5173",
@@ -20,22 +25,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class Image(BaseModel):
-    img: str
-
-    def getImg(self):
-        return self.img
-
-def writeImg(file,img):
-    with open(file, 'wb') as f:
-        f.write(img)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.post("/recognition")
-def recognition(item: Image):
-    img_bytes = base64.b64decode(item.getImg().encode('utf-8'))
-    writeImg('img/img.jpg', img_bytes)
-    return {"img": item.getImg()}
+def recognition(item: RecognitionHttp):
+    writeImg('img/img.jpg', base64.b64decode(item.getImg().encode('utf-8')))
+    name = recog.recognition('img/img.jpg')
+    books = system_recomendation.get_recomendations(name)
+    con = sql.connect("recomen.db")
+    cursor = con.cursor()
+    book_res = []
+    for book in books:
+        res = cursor.execute(
+            'SELECT titulo, autor, editorial, genero, link2 FROM libros WHERE titulo = "'
+            + book[0]
+            + '"'
+        )
+        for row in res:
+            book = {
+                "title": row[0],
+                "author": row[1],
+                "publisher": row[2],
+                "genre": row[3],
+                "link": row[4],
+            }
+            book_res.append(book)
+    
+    user = cursor.execute( 'SELECT * FROM usuarios WHERE codigo = "'+name+'" ')
+    for row in user:
+        user = {
+            "code": row[0],
+            "name": row[1],
+            "email": row[2],
+        }
+    cursor.close()
+    con.close()
+    
+    return {"user": user, "books": book_res}
